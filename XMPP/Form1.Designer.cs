107fad9195e542b5ace9458bd8a732fa845c6f19
Ahmad Syafrie Sabiun <syafrie007@gmail.com>
2021-05-18 08:47:53 +0700

    asd

--- XMPP/Form1.Designer.cs
+++ XMPP/Form1.Designer.cs
@@ -29,13 +29,189 @@ namespace XMPP
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rtf = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(346, 33);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(346, 62);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDiconnect_Click);
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(98, 30);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(242, 20);
+            this.txtServer.TabIndex = 2;
+            this.txtServer.Text = "xmpp.jp";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(98, 56);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(242, 20);
+            this.txtUsername.TabIndex = 2;
+            this.txtUsername.Text = "kiosgamer";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(98, 80);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(242, 20);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.Text = "zxcvbnm1234567";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Password";
+            // 
+            // rtf
+            // 
+            this.rtf.Location = new System.Drawing.Point(27, 106);
+            this.rtf.Name = "rtf";
+            this.rtf.Size = new System.Drawing.Size(394, 367);
+            this.rtf.TabIndex = 4;
+            this.rtf.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 506);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Message";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 482);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Username";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(98, 503);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(242, 75);
+            this.txtMessage.TabIndex = 5;
+            this.txtMessage.Text = "as";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(98, 479);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(242, 20);
+            this.txtTo.TabIndex = 6;
+            this.txtTo.Text = "syafrie007@xmpp.jp";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(346, 555);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(578, 622);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.rtf);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox rtf;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Button btnSend;
     }
 }
 
--- XMPP/Form1.cs
+++ XMPP/Form1.cs
@@ -1,20 +1,70 @@
-﻿using System;
+﻿using Artalk.Xmpp.Client;
+using Artalk.Xmpp.Im;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace XMPP
 {
     public partial class Form1 : Form
     {
+
+        ArtalkXmppClient client;
+        SynchronizationContext _sync;
         public Form1()
         {
             InitializeComponent();
+            _sync = SynchronizationContext.Current;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new ArtalkXmppClient(txtServer.Text, txtUsername.Text, txtPassword.Text);
+            client.Message += OnNewMessage;
+        }
+
+        private void OnNewMessage(object sender, MessageEventArgs e)
+        {
+            //MessageBox.Show("Message from <" + e.Jid + ">: " + e.Message.Body);
+
+            _sync.Send(x => {
+                rtf.AppendText("Message from <" + e.Jid + ">: " + e.Message.Body + "\n");
+            }, null);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            client.Connect();
+
+            btnConnect.Enabled = false;
+            btnDisconnect.Enabled = true;
+        }
+
+        private void btnDiconnect_Click(object sender, EventArgs e)
+        {
+            client.Dispose();
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            client.SendMessage(
+                new Artalk.Xmpp.Jid("xmpp.jp", "syafrie007"), txtMessage.Text,type: MessageType.Normal);
+
         }
     }
+
+
+
+
+
 }
